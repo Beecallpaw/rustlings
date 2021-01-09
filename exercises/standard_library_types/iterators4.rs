a9ca3b56268ce644fa255d68bd2fbc83925ccf6e
Bikalpa <bikalpa.thapa@yipl.com.np>
2021-01-09 14:42:43 +0545

    Solutions upto quiz 4

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,12 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    // if num == 0 {
+    //     1
+    // } else {
+    //     num * factorial(num - 1)
+    // }
+    (1..=num).product()
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
